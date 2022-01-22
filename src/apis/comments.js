0325d9585dc744df4e250572e7a685a327d1afe9
johnson <poshenc@gmail.com>
2022-01-22 15:45:48 +0800

    rebuild API helper with interceptors

--- src/apis/comments.js
+++ src/apis/comments.js
@@ -1,18 +1,13 @@
 import { apiHelper } from "../utils/helpers";
-const getToken = () => localStorage.getItem('token')
 
 export default {
   create({ restaurantId, text }) {
     return apiHelper.post('/comments', {
       restaurantId,
       text
-    }, {
-      headers: { Authorization: `Bearer ${getToken()}` }
     })
   },
   delete({ commentId }) {
-    return apiHelper.delete(`/comments/${commentId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.delete(`/comments/${commentId}`)
   },
 }
